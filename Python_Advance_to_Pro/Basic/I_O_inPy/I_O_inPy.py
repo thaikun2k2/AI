beffbca45f00603a3df94a580796889fdb6ee23f
Nguyễn Văn Thái <91445147+thaikun2k2@users.noreply.github.com>
2026-03-22 23:38:53 +0700

    c

--- Python_Advance_to_Pro/Basic/I_O_inPy/I_O_inPy.py
+++ Python_Advance_to_Pro/Basic/I_O_inPy/I_O_inPy.py
@@ -1,4 +1,9 @@
 ###
+# hom nay troi nang dep
+# kien tri ky luat
+# tu ky
+# tinh yeu 
+# phan boi trung thanh
 
 # ##các cách input
 # Input: 
@@ -19,14 +24,32 @@
 #  --n là số ký tự trong file mình muốn đọc, bỏ trống n sẽ đọc đến hết
 
 
-fread = open("I_O_inPy/Eg.txt", "r")
-# print(fread)
-# print(fread.read(), "\n") ## read tất cả file
+fread = open("I_O_inPy/Eg.txt", "r", encoding= "utf-8") ##encoding= "utf-8" cần dùng khi nếu trg file có ký tự tiếng việt sẽ ko đọc đc, và sẽ cần mã hóa về dạng tiếng việt để đọc ra thành công
+# # print(fread)
+# # print(fread.read(), "\n") ## read tất cả file
 
-## đọc n ký tự trong file 
-# print(fread.read(30))## read 30 ký tự
+# ## đọc n ký tự trong file 
+# # print(fread.read(30))## read 30 ký tự
+
+# ## bỏ trống n sẽ đọc đến hết
+# print(fread.read(10), "\n") # đọc 10 ký tự đầu
+
+# #Name_file_object.tell() vị trí con trỏ trong file hiện tại ở đâu
+# print(fread.tell(), "\n")## vị trí thứ 10 --> trả về vị trí của fill pointer
+# #Name_file_object.seek(n) thay đổi vị trí con trỏ hiên tại trong file
+# # đưa file pointer về vị trí n
+# print(fread.seek(0), "\n")
+
+# print(fread.read(), "\n")## đọc từ vị trí hiện tại đến hết
+# Name_file_object.close() đóng file
+# fread.close()#đóng file
+#eg:
+# print(fread.read(), "\n")# do đã đóng file nên sẽ bị lỗi
+
 
 ## bỏ trống n sẽ đọc đến hết
-print(fread.read(10), "\n") # đọc 10 ký tự đầu
-print(fread.read())## đọc từ vị trí vừa đọc đến hết
+print(fread.read(15), "\n") # đọc 15 ký tự đầu
 
+#Name_file_object.tell() vị trí con trỏ trong file hiện tại ở đâu
+print(fread.tell(), "\n")## vị trí thứ 20 --> trả về vị trí của fill pointer do là ký tự tiếng việt
+print(fread.read(), "\n")## đọc từ vị trí hiện tại đến hết
