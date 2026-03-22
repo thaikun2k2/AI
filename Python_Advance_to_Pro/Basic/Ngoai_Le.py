dbd8e809596d8ddaf29b518649aaaae49692b23b
Nguyễn Văn Thái <91445147+thaikun2k2@users.noreply.github.com>
2026-03-23 01:46:02 +0700

    Update Ngoai_Le.py

--- Python_Advance_to_Pro/Basic/Ngoai_Le.py
+++ Python_Advance_to_Pro/Basic/Ngoai_Le.py
@@ -3,5 +3,8 @@
 ###==> xảy ra 1 ngoại lệ (điều ko mong muốn) đây là 1 lỗi ==>> tiến trình bị tạm dừng ==>> để xử lý ngoại lệ
 
 ## Trg python: 1 ngoại lệ là 1 đối tượng, đại diện cho 1 lỗi
-##Bình thường: run program ==>> xảy ra 1 lỗi 
+##Bình thường: run program ==>> xảy ra 1 lỗi ==>> báo lỗi ==>> kết thúc program
+
+## nếu dùng ngoại lệ: run program ==>> xảy ra 1 lỗi ==>> dừng program ==>> xủ lý lỗi ==>> chạy tiếp
+
 
