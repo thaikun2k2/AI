ac502306f3e3b94858f3a268b5930ecc7e66e7f4
ADMIN <thaikun2k2@gmail.com>
2026-03-29 04:47:59 +0700

    Update OOP.py

--- Python_Advance_to_Pro/Basic/src/OOP.py
+++ Python_Advance_to_Pro/Basic/src/OOP.py
@@ -1,2 +1,13 @@
 ## OOP == ObjectOrientedProgramming
 
+class sinhvien():
+    def __init__(self, hoten, m_sv):
+        self.name = hoten
+    self.msv = m_sv
+        pass
+
+
+
+sv1 = sinhvien()
+
+print(sv1)
