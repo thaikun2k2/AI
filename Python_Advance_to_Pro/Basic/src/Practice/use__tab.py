8875f12c5a5aafcae38b6926d2a5d5e646beb42b
ADMIN <thaikun2k2@gmail.com>
2026-03-30 03:09:52 +0700

    Create use__tab.py

---
+++ Python_Advance_to_Pro/Basic/src/Practice/use__tab.py
@@ -0,0 +1,46 @@
+# print("2 năm nghĩa vụ tạm xa nhà")
+# print("\tKhi về phố thị nở đầy hoa")
+# print("\t\tNgười yêu ra đón cùng con nhỏ")
+# print("Chào chú đi con ...bạn má nè!")
+
+#print("2 năm nghĩa vụ tạm xa nhà", "\n\tKhi về phố thị nở đầy hoa", "\n\t\tNgười yêu ra đón cùng con nhỏ", "\nChào chú đi con ...bạn má nè!")
+
+
+
+
+# from cmath import pi
+# import sys
+# print(sys.version[0:7])
+
+
+# n = int(input("Bán kính hình tròn: "))
+
+# tron = pi * (n**2)
+# print("Diện tích hình tròn là:", tron)
+
+
+
+#chuyển đổi dạng ngày tháng năm
+
+# n = (30, 3, 2026)
+# print("{}/{}/{}".format(n[0], n[1], n[2]))
+# print("%i/%i/%i" % (30, 3, 2026))
+
+
+
+# n = input("Nhập số nguyên cách nhau bởi dấu , là: ")
+# a = n.split(",")
+# lists = []
+# tuples = ()
+# sets = {}
+# for i in a:
+#     lists.append(str(i))
+#     tuples = tuple(lists)
+# print(lists)
+# print(tuples)
+
+
+
+# files = input("nhập vào tên file: ")
+# a = files.split(".")
+# print("đuôi mở rộng là: ", a[-1])
