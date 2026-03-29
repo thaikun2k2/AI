ecf82171262a62903ad54d24d8539c5fd8a02965
ADMIN <thaikun2k2@gmail.com>
2026-03-30 02:37:41 +0700

    Update Sum_binhphuong.py

--- Python_Advance_to_Pro/Basic/src/Practice/Sum_binhphuong.py
+++ Python_Advance_to_Pro/Basic/src/Practice/Sum_binhphuong.py
@@ -1,9 +1,12 @@
 n = int(input("Nhập n: "))
+# def Sum_binhphuong(n):
+#     sum = 0
+#     for i in range(1, n + 1):
+#         sum += i**2
+#     return sum
+
 def Sum_binhphuong(n):
-    sum = 0
-    for i in range(1, n + 1):
-        sum += i**2
-    
-    return sum
+    return n * (n + 1) * (2 * n + 1) // 6
+
 
 print("Tổng bình phương của n là: ", Sum_binhphuong(n))
