24ef81ed08d13272bdcdc30d13ecaf4492d87699
ADMIN <thaikun2k2@gmail.com>
2026-03-30 03:19:57 +0700

    Update use__tab.py

--- Python_Advance_to_Pro/Basic/src/Practice/use__tab.py
+++ Python_Advance_to_Pro/Basic/src/Practice/use__tab.py
@@ -44,3 +44,35 @@
 # files = input("nhập vào tên file: ")
 # a = files.split(".")
 # print("đuôi mở rộng là: ", a[-1])
+
+# ## cách xem chức năng của lệnh là: "lệnh.__doc___"
+# print(abs.__doc__)
+# print(str.__doc__)
+
+
+
+# import calendar
+# y = int(input("nhập vào năm: "))
+# m = int(input("Nhập vào tháng: "))
+# lichthang = calendar.month(y, m)
+# print(lichthang)
+
+# # nhập vào năm: 2026
+# # Nhập vào tháng: 3
+# #      March 2026
+# # Mo Tu We Th Fr Sa Su
+# #                    1
+# #  2  3  4  5  6  7  8
+# #  9 10 11 12 13 14 15
+# # 16 17 18 19 20 21 22
+# # 23 24 25 26 27 28 29
+# # 30 31
+
+
+
+#in số ngày giữa 2 sự kiện
+import datetime
+sk1 = datetime.date(2026,4,30)
+sk2 = datetime.date(2026,3,30)
+delta = sk1 - sk2
+print(delta)
