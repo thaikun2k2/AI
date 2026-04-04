66a8067d482493f9a2e0a360cfb00bb878946e0e
ADMIN <thaikun2k2@gmail.com>
2026-04-04 22:03:51 +0700

    Update def_check_str.py

--- Python_Advance_to_Pro/Basic/src/Practice/def_check_str.py
+++ Python_Advance_to_Pro/Basic/src/Practice/def_check_str.py
@@ -85,16 +85,16 @@
 
 
 
-string1, s2, copy1 = str(input(" Nhập vào string, số ký tự đầu, số lần copy : ")).split(",")
+# string1, s2, copy1 = str(input(" Nhập vào string, số ký tự đầu, số lần copy : ")).split(",")
 
-s1 = int(s2)
-copy = int(copy1)
+# s1 = int(s2)
+# copy = int(copy1)
 
-def copy_kytu(string1, s1, copy):
-    ketqua = ""
-    for i in range(copy):
-        ketqua += string1[0:s1]
-    return ketqua
-xx = copy_kytu(string1, s1, copy)
+# def copy_kytu(string1, s1, copy):
+#     ketqua = ""
+#     for i in range(copy):
+#         ketqua += string1[0:s1]
+#     return ketqua
+# xx = copy_kytu(string1, s1, copy)
 
-print(xx)
+# print(xx)
