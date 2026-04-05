a205dafa31eadd56b3acf549d5e64329eb3e44cd
ADMIN <thaikun2k2@gmail.com>
2026-04-05 21:28:22 +0700

    r

---
+++ Python_Advance_to_Pro/DSA/print_items.py
@@ -0,0 +1,26 @@
+
+# def print_items(n):
+#     for i in range(n):
+#         print(i)
+        
+# # DO NOT CHANGE THIS LINE:
+# print_items(10)
+
+
+# def print_items(n):
+#     for i in range(n):
+#         print(i)
+    
+#     for j in range(n):
+#         print(j)
+# # DO NOT CHANGE THIS LINE:
+# print_items(10)
+
+
+
+def print_items(n):
+    for i in range(n):    
+        for j in range(n):
+            print(i,j)
+# DO NOT CHANGE THIS LINE:
+print_items(10)
