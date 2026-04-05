4076eab4cec4eec237287743585e9f3c25535c8f
ADMIN <thaikun2k2@gmail.com>
2026-04-05 23:43:42 +0700

    Update BIG-O.py

--- Python_Advance_to_Pro/DSA/src/BIG-O.py
+++ Python_Advance_to_Pro/DSA/src/BIG-O.py
@@ -18,6 +18,7 @@
 
 
 ### vòng lặp lồng nhau dộ phức tạp 2 for = n*n=n**2
+## độ phức tạp O(n**2)
 # def print_items(n):
 #     for i in range(n):    
 #         for j in range(n):
@@ -26,11 +27,25 @@
 # print_items(10)
 
 
-## độ phức tạp n**3
-def print_items(n):
-    for i in range(n):    
-        for j in range(n):
-            for k in range(n):
-                print(i,j,k)
-# DO NOT CHANGE THIS LINE:
-print_items(10)
+# ## độ phức tạp O(n**3)
+# def print_items(n):
+#     for i in range(n):    
+#         for j in range(n):
+#             for k in range(n):
+#                 print(i,j,k)
+# # DO NOT CHANGE THIS LINE:
+# print_items(10)
+
+
+# ## độ phức tạp O(n**2 + n)
+# def print_items(n):
+#     for i in range(n):    
+#         for j in range(n):
+#             print(i,j)
+#     for k in range(n):
+#         print(k)
+# # DO NOT CHANGE THIS LINE:
+# print_items(10)
+
+
+
